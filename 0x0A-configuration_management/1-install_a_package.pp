7e032b96fea9e24fcebc250ca1bd6e83e4e6031c
Sakaisha <aishasakkary@outlook.com>
2024-05-08 15:34:10 -0700

    fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
-#!/usr/bin/pup
-# install flask from pip3.
-package { 'flask':
+# This resource installs a package called flask
+
+package {'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
+
+package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
