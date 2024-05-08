0d6502bf4bcea0f05598e7b83d057ad78b50cfbb
Sakaisha <aishasakkary@outlook.com>
2024-05-08 16:28:04 -0700

    idk

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,52 @@
+# Using puppet to manage my nginx configurations
+
+# Install Nginx package
+package { 'nginx':
+  ensure => installed,
+}
+
+# Configure the Nginx Server
+file { '/etc/nginx/sites-available/default':
+  ensure  => present,
+  content => "
+    server {
+	    listen 80;
+		listen [::]:80;
+		
+		server_name 54.237.9.122;
+
+		root /var/www/html;
+		index index.html index.htm index.nginx-debian.html;
+
+		location / {
+			return 200 'Hello World!';
+		}
+
+		location /redirect_me {
+			return 301 'https://www.youtube.com/watch?v=QH2-TGUlwu4';
+		}
+	}
+  ",
+    notify => Exec['nginx_restart'],
+}
+
+# Enable the default site
+file { '/etc/nginx/sites-enabled/default':
+  ensure  => link,
+  target  => '/etc/nginx/sites-available/default',
+  require => File['/etc/nginx/sites-available/default'],
+}
+
+# Ensure Nginx server is always running or restart
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => Package['nginx'],
+}
+
+# Execute command to restart Nginx
+exec { 'nginx_restart':
+  command     => 'service nginx restart',
+  path        => '/usr/sbin:/usr/bin:/sbin:/bin',
+  refreshonly => true,
+}
